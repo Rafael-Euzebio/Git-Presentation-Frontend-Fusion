bebca162cb8eaa6c0e6629e314ea00c49c65fafd
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-11-12 21:53:41 -0300

    feat: adiciona navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <navbar>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </navbar>
+  )
+}
+
